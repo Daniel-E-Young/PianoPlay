c3df2f84017dcfb738a045a889cd84cf1159e275
Daniel-E-Young <youngdanielebi@yahoo.com>
2024-02-12 10:06:04 +0100

    commi

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ audio = new Audio("tunes/a.wav"); // by default, audio src will be the "a" tune
 
 const playTune = (key) => {
     audio.src = 'tunes/${key}.wav' // passing audio src based on keys pressed
-    audio
+    audio.play(); // play audio
 }
