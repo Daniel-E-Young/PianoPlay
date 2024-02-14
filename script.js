92b2fa027b18f8fb6c2c85e0d7a23d4422fe1c10
Daniel-E-Young <youngdanielebi@yahoo.com>
2024-02-14 12:10:18 +0100

    commi

--- script.js
+++ script.js
@@ -19,4 +19,5 @@ const playTune = (key) => {
 keyboardKeys.forEach(key => {
     allKeys.push(key.dataset.key); 
     key.addEventListener("click",() => playTune(key.dataset.key));
+    consoleKey
 });
